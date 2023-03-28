5c424761c07b5f7ccbece431102a53dd9df38755
Karam <krmhmade1@gmail.com>
2023-03-28 12:42:41 +0300

    feat: add all endpoints's classes to __init__.py

---
+++ pytorjoman/__init__.py
@@ -0,0 +1,5 @@
+from pytorjoman.accounts import Account
+from pytorjoman.projects import Project
+from pytorjoman.sections import Section
+from pytorjoman.sentences import Sentence
+from pytorjoman.translations import Translation
